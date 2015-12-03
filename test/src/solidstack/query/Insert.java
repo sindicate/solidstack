3bd6ec2e5dae77e8143fb63a3609a410a9590d16
René <rene.de.bloois@gmail.com>
2015-12-03 21:04:49 +0100

    Need to use Java 1.8 because TestNG does not work anymore with Java 1.6. Added test that inserts a byte[] and an InputStream. Added test that inserts a Reader.

---
+++ test/src/solidstack/query/Insert.java
@@ -0,0 +1,77 @@
+package solidstack.query;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.testng.annotations.Test;
+
+import solidstack.util.Pars;
+
+public class Insert
+{
+	@Test
+	public void testInsertByteArray() throws ClassNotFoundException, SQLException
+	{
+		Class.forName( "org.apache.derby.jdbc.EmbeddedDriver" );
+		Connection connection = DriverManager.getConnection( "jdbc:derby:memory:test;create=true", "app", null );
+
+		connection.createStatement().executeUpdate( "CREATE TABLE PICTABLE ( PIC BLOB )" );
+
+		QueryLoader queries = new QueryLoader();
+		queries.setTemplatePath( "classpath:/solidstack/query" );
+
+		Query query = queries.getQuery( "insertBlob.sql" );
+		byte[] bytes = new byte[ 1000 ];
+		query.update( connection, new Pars( "pic", bytes ) );
+		query.update( connection, new Pars( "pic", new ByteArrayInputStream( bytes ) ) );
+
+		ResultSet result = connection.createStatement().executeQuery( "SELECT * FROM PICTABLE" );
+		assertThat( result.next() ).isTrue();
+
+		bytes = result.getBytes( 1 );
+		assertThat( bytes ).hasSize( 1000 );
+
+		assertThat( result.next() ).isTrue();
+
+		bytes = result.getBytes( 1 );
+		assertThat( bytes ).hasSize( 1000 );
+
+		assertThat( result.next() ).isFalse();
+	}
+
+	@Test
+	public void testInsertReader() throws ClassNotFoundException, SQLException
+	{
+		Class.forName( "org.apache.derby.jdbc.EmbeddedDriver" );
+		Connection connection = DriverManager.getConnection( "jdbc:derby:memory:test;create=true", "app", null );
+
+		connection.createStatement().executeUpdate( "CREATE TABLE TEXTTABLE ( TEXT CLOB )" );
+
+		QueryLoader queries = new QueryLoader();
+		queries.setTemplatePath( "classpath:/solidstack/query" );
+
+		Query query = queries.getQuery( "insertClob.sql" );
+		String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		query.update( connection, new Pars( "text", text ) );
+		query.update( connection, new Pars( "text", new StringReader( text ) ) );
+
+		ResultSet result = connection.createStatement().executeQuery( "SELECT * FROM TEXTTABLE" );
+		assertThat( result.next() ).isTrue();
+
+		text = result.getString( 1 );
+		assertThat( text ).hasSize( 26 );
+
+		assertThat( result.next() ).isTrue();
+
+		text = result.getString( 1 );
+		assertThat( text ).hasSize( 26 );
+
+		assertThat( result.next() ).isFalse();
+	}
+}
